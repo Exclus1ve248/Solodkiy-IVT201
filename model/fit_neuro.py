c781a7d11df7e89d8022b7da841a6b0981e25ac6
exclus1ve <solodkijnikita@gmail.com>
2024-09-24 19:24:32 +0300

    final_ver

--- model/fit_neuro.py
+++ model/fit_neuro.py
@@ -1,14 +1,11 @@
 import numpy as np
 from neuron import SingleNeuron
 
-
-# Пример данных (X - входные данные, y - целевые значения)
-X = np.array([  [-2, -1, -4],
-                [25, 6, 4],
-                [17, 4, 2],
-                [-15, -6, -6]])
-y = np.array([1, 0, 0, 1])  # Ожидаемый выход
-# Инициализация и обучение нейрона
+X = np.array([  [-2, -1, 0],
+                [25, 6, 7],
+                [17, 4, 6],
+                [-15, -6, 1]])
+y = np.array([1, 0, 0, 1])
 neuron = SingleNeuron(input_size=3)
 neuron.train(X, y, epochs=1000, learning_rate=0.1)
 
