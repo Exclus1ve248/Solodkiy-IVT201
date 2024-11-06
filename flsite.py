6cd5118c480eb132e475ba5a329c7b9c2031dfdc
exclus1ve248 <solodkijnikita@gmail.com>
2024-11-06 14:24:52 +0300

    lab18

--- flsite.py
+++ flsite.py
@@ -1,18 +1,28 @@
+import io
 import pickle
 
 import tensorflow as tf
 import numpy as np
+import os
 from flask import Flask, render_template, url_for, request, jsonify
+import matplotlib.pyplot as plt
+# from keras.api.preprocessing import image
+from PIL import Image
+from keras.api.preprocessing import image
 
 from model.neuron import SingleNeuron
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'photo'
 
 menu = [{"name": "Лаба 1", "url": "p_lab1"},
         {"name": "Лаба 2", "url": "p_lab2"},
         {"name": "Лаба 3", "url": "p_lab3"},
         {"name": "Лаба 4", "url": "p_lab4"},
-        {"name": "Лаба 5", "url": "p_lab5"}]
+        {"name": "Лаба 5", "url": "p_lab5"},
+        {"name": "Лаба 18", "url": "p_lab18"}]
+
+arr = ["футболка", "брюки", "кофта", "платье", "пальто", "сандали", "рубашка", "кроссовок", "сумка", "ботинок"]
 
 loaded_model_lin_reg = pickle.load(open('model/lin_reg', 'rb'))
 loaded_model_log_reg = pickle.load(open('model/log_reg', 'rb'))
@@ -21,6 +31,7 @@ loaded_model_tree = pickle.load(open('model/tree', 'rb'))
 new_neuron = SingleNeuron(input_size=2)
 new_neuron.load_weights('model/neuron_weights.txt')
 model_class = tf.keras.models.load_model('model/classification_model.h5')
+model_fashion = tf.keras.models.load_model('model/model_fashion.h5')
 
 @app.route('/api', methods=['get'])
 def get_weather():
@@ -113,6 +124,33 @@ def predict_classification():
     print(result)
     app.config['JSON_AS_ASCII'] = False
     return jsonify(predict = str(result))
+
+@app.route('/p_lab18')
+def upload_form():
+    return render_template('lab18.html')
+
+@app.route('/p_lab18', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'Нет файла для загрузки!'
+    file = request.files['file']
+    if file.filename == '':
+        return 'Выберите файл!'
+    if file:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        img = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        img = image.load_img(img, target_size=(28, 28), color_mode="grayscale")
+        x = image.img_to_array(img)
+        x = x.reshape((1, 28, 28, 1))
+        x = x.reshape(1, 784)
+        x /= 255
+        prediction = np.argmax(model_fashion.predict(x))
+        return render_template('lab18.html', title="Нейрон для классификации одежды", menu=menu,
+                               class_model=arr[prediction])
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
